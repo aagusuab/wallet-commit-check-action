eff73563cb7d6b9eeb140fceabeb007557463763
Youn Jae Kim <younjaekim@microsoft.com>
2022-09-19 20:56:34 -0700

    pushing fix for the action v1.7

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import * as core from "@actions/core";
-import * as commitMessages from '/commitMessage';
+import * as commitMessages from './commitMessage';
 
 
-try {
-    const messages = commitMessages.retrieve();
-    const messageLength = messages.length
-    for (let i = 0; i < messageLength; i++) {
-        console.log(messages[i]);
+    try {
+        const messages = commitMessages.retrieve();
+        const messageLength = messages.length
+        for (let i = 0; i < messageLength; i++) {
+            console.log(messages[i]);
+        }
+        // core.debug(`Commit Message Found:\n${message}`);
+
+    } catch (error) {
+        core.setFailed(error.message);
     }
-    // core.debug(`Commit Message Found:\n${message}`);
-
-} catch (error) {
-    core.setFailed(error.message);
-}
