4301eef5396b96c9a439443826da04f20b6b2515
Youn Jae Kim <younjaekim@microsoft.com>
2022-09-20 09:11:42 -0700

    pushing fix for the action v1.9

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import * as core from "@actions/core";
-
+import {retrieve} from '/commitMessage.js'
 
 try {
-    const messages = require('./commitMessage.js').retrieve();
+    const messages = retrieve();
     const messageLength = messages.length
     for (let i = 0; i < messageLength; i++) {
         console.log(messages[i]);
