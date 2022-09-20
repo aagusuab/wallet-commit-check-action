8df3ef7790e2129f75f1ad00cc96716313762915
Youn Jae Kim <younjaekim@microsoft.com>
2022-09-19 19:04:42 -0700

    pushing fix for the action

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
-    const message = github.context.payload.commits.message
+    // const message = github.context.payload.commits.message
+    const message = github.event.head_commit.message
 
     // `who-to-greet` input defined in action metadata file
     console.log(`The commit message is ${message}`);
