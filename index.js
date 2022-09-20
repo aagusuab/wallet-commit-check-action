e8d03d1b67c2f0898cafd0b5ebf3215fd241d8b2
Youn Jae Kim <younjaekim@microsoft.com>
2022-09-19 20:16:02 -0700

    pushing fix for the action v1.4

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
+import * as commitMessages from '/commitMessage';
 
 try {
-    // const message = github.context.payload.commits.message
-    const message = github.event.head_commit.message
-
-    // `who-to-greet` input defined in action metadata file
-    console.log(`The commit message is ${message}`);
+    // const message = github.event.head_commit.message
+    const messages: string[] = commitMessages.retrieve();
+    const messageLength = messages.length
+    for (let i = 0; i < messageLength; i++){
+        console.log(messages[i]);
+    }
+    // core.debug(`Commit Message Found:\n${message}`);
 
 } catch (error) {
     core.setFailed(error.message);
 }
-
-
-
