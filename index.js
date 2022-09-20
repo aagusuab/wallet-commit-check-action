63f9e45b98a451b73f58176d732957805ace16f7
Youn Jae Kim <younjaekim@microsoft.com>
2022-09-19 20:52:10 -0700

    pushing fix for the action v1.5

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const core = require('@actions/core');
-import {retrieve} from '/src/commitMessage';
+import * as core from "@actions/core";
+import * as commitMessages from '/commitMessage';
+
 
 try {
-    // const message = github.event.head_commit.message
-    const messages: string[] = retrieve();
+    const messages = commitMessages.retrieve();
     const messageLength = messages.length
-    for (let i = 0; i < messageLength; i++){
+    for (let i = 0; i < messageLength; i++) {
         console.log(messages[i]);
     }
     // core.debug(`Commit Message Found:\n${message}`);
